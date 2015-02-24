a0c8113b78b933cb6cf7062b1ae18918d62a45c6
rubenwardy <rubenwardy@gmail.com>
2015-02-24 09:37:43 +0000

    Update support list

--- init.lua
+++ init.lua
@@ -144,8 +144,25 @@ if minetest.get_modpath("farming") ~= nil then
 end
 
 if minetest.get_modpath("mobs") ~= nil then
-	overwrite("mobs:meat", 6)
-	overwrite("mobs:rat_cooked", 5)
+	if mobs.mod ~= nil and mobs.mod == "redo" then
+		overwrite("mobs:cheese", 4)
+		overwrite("mobs:meat", 8)
+		overwrite("mobs:meat_raw", 4)
+		overwrite("mobs:rat_cooked", 4)
+		overwrite("mobs:honey", 2)
+		overwrite("mobs:pork_raw", 3, "", 3)
+		overwrite("mobs:pork_cooked", 8)
+		overwrite("mobs:chicken_cooked", 6)
+		overwrite("mobs:chicken_raw", 2, "", 3)
+		overwrite("mobs:chicken_egg_fried", 2)
+		if minetest.get_modpath("bucket") then 
+			overwrite("mobs:bucket_milk", 3, "bucket:bucket_empty")
+		end
+	else
+		overwrite("mobs:meat", 6)
+		overwrite("mobs:meat_raw", 3)
+		overwrite("mobs:rat_cooked", 5)
+	end
 end
 
 if minetest.get_modpath("moretrees") ~= nil then
@@ -174,6 +191,8 @@ if minetest.get_modpath("animalmaterials") ~= nil then
 	overwrite("animalmaterials:meat_chicken", 3)
 	overwrite("animalmaterials:meat_lamb", 3)
 	overwrite("animalmaterials:meat_venison", 3)
+	overwrite("animalmaterials:meat_undead", 3, "", 3)
+	overwrite("animalmaterials:meat_toxic", 3, "", 5)
 	overwrite("animalmaterials:meat_ostrich", 3)
 	overwrite("animalmaterials:fish_bluewhite", 2)
 	overwrite("animalmaterials:fish_clownfish", 2)
@@ -181,7 +200,7 @@ end
 
 if minetest.get_modpath("fishing") ~= nil then
 	overwrite("fishing:fish_raw", 2)
-	overwrite("fishing:fish", 4)
+	overwrite("fishing:fish_cooked", 5)
 	overwrite("fishing:sushi", 6)
 	overwrite("fishing:shark", 4)
 	overwrite("fishing:shark_cooked", 8)
@@ -204,16 +223,15 @@ end
 if minetest.get_modpath("bushes_classic") then
 	-- bushes_classic mod, as found in the plantlife modpack
 	local berries = {
-		"strawberry",
+	    "strawberry",
 		"blackberry",
 		"blueberry",
 		"raspberry",
 		"gooseberry",
-		"mixed_berry"
-	}
+		"mixed_berry"}
 	for _, berry in ipairs(berries) do
 		if berry ~= "mixed_berry" then
-		
+			overwrite("bushes:"..berry, 1)
 		end
 		overwrite("bushes:"..berry.."_pie_raw", 2)
 		overwrite("bushes:"..berry.."_pie_cooked", 5)
@@ -224,12 +242,17 @@ end
 if minetest.get_modpath("mushroom") ~= nil then
 	overwrite("mushroom:brown", 1)
 	overwrite("mushroom:red", 1, "", 3)
+	-- mushroom potions: red = strong poison, brown = light restorative
+	if minetest.get_modpath("vessels") then
+		overwrite("mushroom:brown_essence", 1, "vessels:glass_bottle", nil, 4)
+		overwrite("mushroom:poison", 1, "vessels:glass_bottle", 10)
+	end
 end
 
 if minetest.get_modpath("docfarming") ~= nil then
-	overwrite("docfarming:carrot", 2)
+	overwrite("docfarming:carrot", 3)
 	overwrite("docfarming:cucumber", 2)
-	overwrite("docfarming:corn", 2)
+	overwrite("docfarming:corn", 3)
 	overwrite("docfarming:potato", 4)
 	overwrite("docfarming:bakedpotato", 5)
 	overwrite("docfarming:raspberry", 3)
@@ -308,21 +331,109 @@ if minetest.get_modpath("mobfcooking") ~= nil then
 	overwrite("mobfcooking:cooked_fish", 6)
 end
 
-if minetest.get_modpath("ethereal") ~= nil then
+if minetest.get_modpath("creatures") ~= nil then
+	overwrite("creatures:meat", 6)
+	overwrite("creatures:flesh", 3)
+	overwrite("creatures:rotten_flesh", 3, "", 3)
+end
+
+if minetest.get_modpath("ethereal") then
 	overwrite("ethereal:strawberry", 1)
-	overwrite("ethereal:banana", 2)
+	overwrite("ethereal:banana", 4)
 	overwrite("ethereal:pine_nuts", 1)
+	overwrite("ethereal:bamboo_sprout", 0, "", 3)
 	overwrite("ethereal:fern_tubers", 1)
 	overwrite("ethereal:banana_bread", 7)
-	overwrite("ethereal:mushroom_plant", 1)
+	overwrite("ethereal:mushroom_plant", 2)
 	overwrite("ethereal:coconut_slice", 2)
-	overwrite("ethereal:golden_apple", 10)
-	overwrite("ethereal:wild_onion_plant", 1)
-	overwrite("ethereal:mushroom_soup", 2)
-	overwrite("ethereal:mushroom_soup_cooked", 3)
-	overwrite("ethereal:hearty_stew", 4)
-	overwrite("ethereal:hearty_stew_cooked", 6)
-	overwrite("ethereal:bucket_cactus", 2)
+	overwrite("ethereal:golden_apple", 4, "", nil, 10)
+	overwrite("ethereal:wild_onion_plant", 2)
+	overwrite("ethereal:mushroom_soup", 4, "ethereal:bowl")
+	overwrite("ethereal:mushroom_soup_cooked", 6, "ethereal:bowl")
+	overwrite("ethereal:hearty_stew", 6, "ethereal:bowl", 3)
+	overwrite("ethereal:hearty_stew_cooked", 10, "ethereal:bowl")
+	if minetest.get_modpath("bucket") then
+		overwrite("ethereal:bucket_cactus", 2, "bucket:bucket_empty")
+	end
 	overwrite("ethereal:fish_raw", 2)
-	overwrite("ethereal:fish_cooked", 4)
+	overwrite("ethereal:fish_cooked", 5)
+	overwrite("ethereal:seaweed", 1)
+	overwrite("ethereal:yellowleaves", 1, "", nil, 1)
+	overwrite("ethereal:sashimi", 4)
+end
+
+if minetest.get_modpath("farming") and farming.mod == "redo" then
+	overwrite("farming:bread", 6)
+	overwrite("farming:potato", 1)
+	overwrite("farming:baked_potato", 6)
+	overwrite("farming:cucumber", 4)
+	overwrite("farming:tomato", 4)
+	overwrite("farming:carrot", 3)
+	overwrite("farming:carrot_gold", 6, "", nil, 8)
+	overwrite("farming:corn", 3)
+	overwrite("farming:corn_cob", 5)
+	overwrite("farming:melon_slice", 2)
+	overwrite("farming:pumpkin_slice", 1)
+	overwrite("farming:pumpkin_bread", 9)
+	overwrite("farming:coffee_cup", 2, "farming:drinking_cup")
+	overwrite("farming:coffee_cup_hot", 3, "farming:drinking_cup", nil, 2)
+	overwrite("farming:cookie", 2)
+	overwrite("farming:chocolate_dark", 3)
+	overwrite("farming:donut", 4)
+	overwrite("farming:donut_chocolate", 6)
+	overwrite("farming:donut_apple", 6)
+	overwrite("farming:raspberries", 1)
+	overwrite("farming:blueberries", 1)
+	overwrite("farming:muffin_blueberry", 4)
+	if minetest.get_modpath("vessels") then
+		overwrite("farming:smoothie_raspberry", 2, "vessels:drinking_glass")
+	end
+	overwrite("farming:rhubarb", 1)
+	overwrite("farming:rhubarb_pie", 6)
+end
+
+if minetest.get_modpath("kpgmobs") ~= nil then
+	overwrite("kpgmobs:uley", 3)
+	overwrite("kpgmobs:meat", 6)
+	overwrite("kpgmobs:rat_cooked", 5)
+	overwrite("kpgmobs:med_cooked", 4)
+  	if minetest.get_modpath("bucket") then
+	   overwrite("kpgmobs:bucket_milk", 4, "bucket:bucket_empty")
+	end
+end
+
+if minetest.get_modpath("jkfarming") ~= nil then
+	overwrite("jkfarming:carrot", 3)
+	overwrite("jkfarming:corn", 3)
+	overwrite("jkfarming:melon_part", 2)
+	overwrite("jkfarming:cake", 3)
+end
+
+if minetest.get_modpath("jkanimals") ~= nil then
+	overwrite("jkanimals:meat", 6)
+end
+
+if minetest.get_modpath("jkwine") ~= nil then
+	overwrite("jkwine:grapes", 2)
+	overwrite("jkwine:winebottle", 1)
+end
+
+if minetest.get_modpath("cooking") ~= nil then
+	overwrite("cooking:meat_beef_cooked", 4)
+	overwrite("cooking:fish_bluewhite_cooked", 3)
+	overwrite("cooking:fish_clownfish_cooked", 1)
+	overwrite("cooking:meat_chicken_cooked", 2)
+	overwrite("cooking:meat_cooked", 2)
+	overwrite("cooking:meat_pork_cooked", 3)
+	overwrite("cooking:meat_toxic_cooked", -3)
+	overwrite("cooking:meat_venison_cooked", 3)
+	overwrite("cooking:meat_undead_cooked", 1)
+end
+
+-- ferns mod of plantlife_modpack
+if minetest.get_modpath("ferns") ~= nil then
+	overwrite("ferns:fiddlehead", 1, "", 1)
+	overwrite("ferns:fiddlehead_roasted", 3)
+	overwrite("ferns:ferntuber_roasted", 3)
+	overwrite("ferns:horsetail_01", 1)
 end
